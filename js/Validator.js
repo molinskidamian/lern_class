facd52892c8cccd801b02bb274b840a0456f3b94
Damian Molinski <tedeer@gmail.com>
2021-12-07 00:42:43 +0100

    Creating Validator method .saveUser()

--- js/Validator.js
+++ js/Validator.js
@@ -1,8 +1,9 @@
 class Validator {
     constructor(form) {
         this.form = form;
-        this.fields = this.form.querySelectorAll("[required]");
+        this.fields = this.form.querySelectorAll("input");
         this.errors = [];
+        this.db = [];
     }
 
     info = () => {
@@ -11,9 +12,51 @@ class Validator {
 
     showFields = () => {
         const requiredFields = [...this.fields]; // create for true array
-        requiredFields.forEach((el) => {
-            console.log(el.value);
+        console.log(`--- .showFiles() ---`);
+
+        requiredFields.forEach((el, index) => {
+            console.log(`${el.name}: ${el.value}`);
         });
+    };
+
+    saveUser = () => {
+        this.fields = [...this.fields];
+
+        // if (this.fields[0].value != "") {
+        //     console.log(`Uzupełniono --- Tworzę obiekt`);
+        //     const user = {};
+        // } else {
+        //     console.log("Nie uzupełniono --- Nie mogę utworzyć obiektu");
+        // }
+
+        const user = {};
+
+        for (let i = 0; i < this.fields.length; i++) {
+            user[this.fields[i].name] = this.fields[i].value;
+            // console.log(`${this.fields[i].name}: ${this.fields[i].value}`);
+        }
+
+        this.db.push(user);
+    };
+
+    showUsersTable = () => {
+        console.log(`--- .showUsersTable() ---`);
+        console.log(this.db);
+
+        const usersTable = document.querySelector("table[data-users='table']");
+        usersTable.classList.add("table-success", "table-striped");
+
+        // const tr = document.createElement("tr");
+        // const td = document.createElement("td");
+        // const email = document.createTextNode("damianmolinski@gmail.com");
+
+        // td.appendChild(email);
+        // tr.appendChild(td);
+        // usersTable.appendChild(tr);
+
+        console.log(this.db.length);
+
+        console.log(usersTable);
     };
 }
 
