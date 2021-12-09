25d84f8f2ce98c0b07cbc06bd5f8fcd30f6fd182
Damian Molinski <tedeer@gmail.com>
2021-12-09 23:46:43 +0100

    Finished method .saveUser() in Validator class

--- js/Validator.js
+++ js/Validator.js
@@ -20,35 +20,32 @@ class Validator {
     };
 
     saveUser = () => {
+        // save to Array
         this.fields = [...this.fields];
-
-        // if (this.fields[0].value != "") {
-        //     console.log(`Uzupełniono --- Tworzę obiekt`);
-        //     const user = {};
-        // } else {
-        //     console.log("Nie uzupełniono --- Nie mogę utworzyć obiektu");
-        // }
-
         const user = {};
-
         for (let i = 0; i < this.fields.length; i++) {
             user[this.fields[i].name] = this.fields[i].value;
             // console.log(`${this.fields[i].name}: ${this.fields[i].value}`);
         }
-
         this.db.push(user);
     };
 
     showUsersTable = () => {
-        console.log(`--- .showUsersTable() ---`);
-        console.log(this.db);
-
         const usersTable = document.querySelector("table[data-users='table']");
         usersTable.classList.add("table-success", "table-striped");
 
-        // const tr = document.createElement("tr");
-        // const td = document.createElement("td");
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
         // const email = document.createTextNode("damianmolinski@gmail.com");
+
+        // for (let i = 0; i < widnow.localStorage.length; i++) {
+        //     const text = document.createTextNode(window.localStorage);
+        // }
+
+        // const webSorage = [...window.localStorage];
+
+        // console.log(window.localStorage.getItem.email);
+        // console.log(window.localStorage.getItem.password);
 
         // td.appendChild(email);
         // tr.appendChild(td);
--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 import Validator from "./Validator";
 
+const db = [];
+const aggregate = {
+    id: 1,
+    type: "KLU2",
+    serialNumber: "13_21/2021",
+    electricalCabinet: false,
+    eCSerialNumber: "",
+};
+
 const btn = document.querySelector("button[data-button='addUser']");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const user = new Validator(document.getElementById("addUserForm"));
-    user.showFields();
+    user.saveUser();
+
+    if (window.localStorage.length != 0) {
+        user.showUsersTable();
+        console.log(user.db);
+    }
 });
